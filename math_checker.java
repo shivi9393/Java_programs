9e3913e594028b421cef37517fe099248f11f742
shivi9393 <joshishivdev@gmail.com>
2024-12-23 17:27:10 +0530

    created a nwew program

---
+++ math_checker.java
@@ -0,0 +1,84 @@
+public class math_checker {
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+    
+        System.out.println("enter a for addition");
+        System.out.println("enter b for subtraction");
+        System.out.println("enter c for multipication");
+        System.out.println("enter d for division");
+        System.out.println("enter e for modulus");
+;
+
+        switch (args[0]) {
+            case "a":{
+                int sum=a+b;
+                int inpt=30;
+                // System.out.println("sum of two numbers is:"+sum);
+                if(inpt==sum){
+                    System.out.println("correct");
+                }   
+                else{
+                    System.out.println("incorrect");
+                }
+                break;
+            }
+            case "b":{
+                int sub=a-b;
+                int inpt=-10;
+                if(inpt==sub){
+                    System.out.println("correct");
+                }
+                else{
+                    System.out.println("incorrect");
+                }
+                // System.out.println("subtraction of two numbers is:"+sub);
+                break;
+            }
+            case "c":{
+                int mul=a*b;
+                int inpt = 200;
+                if(inpt==mul){
+                    System.out.println("correct");
+                }   
+                else{
+                    System.out.println("incorrect");
+                }
+
+                // System.out.println("multipication of two numbers is:"+mul);
+                break;
+            }
+            case "d":{
+                int div=b/a;
+                int inpt=2;
+                if(inpt==div){
+                    System.out.println("correct");
+                }
+                else{
+                    System.out.println("incorrect");
+                }
+                // System.out.println("division of two numbers is:"+div);
+                break;
+            }
+            case "e":{
+                int mod=b%a;
+                int inpt=2;
+                if(inpt==mod){
+                    System.out.println("correct");
+                }
+                else{
+                    System.out.println("incorrect");
+                }
+                // System.out.println("modulus of two numbers is:"+mod);
+                break;
+            }
+        
+                
+        
+            
+        }
+     
+
+
+    }
+}
